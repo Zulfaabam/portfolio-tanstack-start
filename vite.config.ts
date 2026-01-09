65d063b297cd3894b5911682971332c7907fa6f6
Zulfaabam <zulfafatahakbar@gmail.com>
2026-01-09 12:50:53 +0700

    feat: add netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
 import viteReact from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+import netlify from '@netlify/vite-plugin-tanstack-start';
 
 export default defineConfig({
   server: { port: 3000 },
@@ -14,5 +15,6 @@ export default defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    netlify(),
   ],
 });
