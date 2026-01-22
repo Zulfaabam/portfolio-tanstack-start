424d050d96a1f044943cbe4869fe5ec23bc55332
Zulfaabam <zulfafatahakbar@gmail.com>
2026-01-22 16:41:47 +0700

    wip: bento layout & content adjustment

--- src/components/sections/bento-grid-section.tsx
+++ src/components/sections/bento-grid-section.tsx
@@ -9,6 +9,7 @@ import { Image } from '@unpic/react';
 import { IconCircleArrowDown, IconCircleArrowRight } from '@tabler/icons-react';
 import FeaturedProjects from './featured-projects';
 import PixelPlayground from '../ui/pixel-playground';
+import BorderMagicBtn from '../ui/border-magic-btn';
 
 export const getTechStack = createServerFn({ method: 'GET' }).handler(
   async (): Promise<{
@@ -25,7 +26,11 @@ export const getTechStack = createServerFn({ method: 'GET' }).handler(
 );
 
 export default function BentoGridSection() {
-  const { data, error, isLoading: loadingTechStack } = useQuery({
+  const {
+    data,
+    error,
+    isLoading: loadingTechStack,
+  } = useQuery({
     queryKey: ['techStack'],
     queryFn: () => getTechStack(),
   });
@@ -38,24 +43,31 @@ export default function BentoGridSection() {
         <BentoBox className='col-span-1 row-span-2 flex h-[576px] flex-col justify-between sm:col-span-2 lg:col-span-9'>
           <div className='space-y-6'>
             <Image
-              src='/abam-working.jpg'
+              src='/abam-working.webp'
               layout='fullWidth'
               className='max-h-[356px] w-[736px] rounded-xl object-cover object-[25%_75%]'
             />
-            <p className='text-justify'>
-              Name’s Zulfa Fatah Akbar Ahmad. You can call me Abam, Frontend
+            <p className='text-justify text-sm leading-6'>
+              Name’s Zulfa Fatah Akbar Ahmad. You can call me Abam, a Frontend
               Developer with 3+ years of experience building production-grade
-              web applications using React, Next.js, TypeScript and any other
-              frontend technologies. Passionate about crafting seamless user
-              experiences and writing clean, efficient code.
+              web applications using React, Next.js, TypeScript, TailwindCSS,
+              and other modern frontend technologies. Strong in problem-solving,
+              with a passion for crafting seamless user experiences and writing
+              clean, efficient, and maintainable code.
             </p>
           </div>
-          <button className='border-fg flex items-center gap-0.5 self-end rounded-full border py-1 pl-2 pr-1 text-sm'>
+          <a
+            className='border-fg flex cursor-pointer items-center gap-0.5 self-end rounded-full border py-1 pl-2 pr-1 text-sm'
+            href='/CV_ZULFA.pdf'
+            target='_blank'
+            rel='noopener noreferrer'
+            download
+          >
             Resume{' '}
             <span>
               <IconCircleArrowDown size={22} strokeWidth={1.5} />
             </span>
-          </button>
+          </a>
         </BentoBox>
         <BentoBox className='col-span-1 row-span-1 flex h-[276px] flex-col justify-between lg:col-span-3'>
           <div className='space-y-6'>
@@ -66,7 +78,7 @@ export default function BentoGridSection() {
                 <div className='text-fg/80 flex items-center gap-2 text-xs'>
                   <p>Jakarta, Indonesia</p>
                   <span className='bg-fg/40 h-3 w-px'></span>
-                  <p>2025 - Now</p>
+                  <p>2025</p>
                 </div>
               </div>
               <div className='space-y-0.5'>
@@ -79,47 +91,37 @@ export default function BentoGridSection() {
               </div>
             </div>
           </div>
-          <button className='border-fg flex items-center gap-0.5 self-end rounded-full border py-1 pl-2 pr-1 text-sm'>
+          <button className='border-fg flex cursor-pointer items-center gap-0.5 self-end rounded-full border py-1 pl-2 pr-1 text-sm'>
             Full Journey{' '}
             <span>
               <IconCircleArrowRight size={22} strokeWidth={1.5} />
             </span>
           </button>
         </BentoBox>
-        <BentoBox className='col-span-1 row-span-1 flex h-[276px] flex-col justify-between lg:col-span-3'>
-          <h6 className='font-medium'>Tech Stack</h6>
-          {loadingTechStack ? <div className='rounded-xl bg-gray-400 w-8 h-4 animate-pulse'></div> : <TechStackBox techStack={techStack} />}
-          {error && <p>Error loading tech stack</p>}
+        <BentoBox className='col-span-1 row-span-1 h-[276px] space-y-6 lg:col-span-3'>
+          <h6 className='font-medium'>What I'm Up to</h6>
+          <div className='space-y-2'>
+            <p className='text-fg flex gap-2 text-sm'>
+              <span>🟢</span> Open to work
+            </p>
+            <p className='text-fg flex gap-2 text-sm'>
+              <span>🌱</span> Now learning: Node.js & Backend techs
+            </p>
+            <p className='text-fg flex gap-2 text-sm'>
+              <span>👨‍💻</span> Working on this portfolio
+            </p>
+          </div>
         </BentoBox>
       </div>
       <div className='text-fg grid w-full auto-cols-min grid-cols-1 gap-4 md:grid-cols-6 lg:grid-cols-12 lg:gap-6'>
-        <BentoBox className='col-span-1 row-span-2 flex h-[180px] flex-col justify-between md:col-span-3'>
-          <h6 className='font-medium'>Latest Strava Activity</h6>
-          <div className='space-y-5'>
-            <div className='flex items-start gap-2'>
-              <Image src='/icons/run.svg' width={20} height={20} />
-              <div>
-                <p className='text-sm'>Outdoor Running</p>
-                <p className='text-fg/80 text-[10px]'>
-                  14 Sept 2025 at 6:00 AM
-                </p>
-              </div>
-            </div>
-            <div className='flex justify-around'>
-              <div>
-                <p className='text-fg/80 text-[11px]'>Distance</p>
-                <p className='text-sm font-medium'>5.00 km</p>
-              </div>
-              <div>
-                <p className='text-fg/80 text-[11px]'>Pace</p>
-                <p className='text-sm font-medium'>6:18 /km</p>
-              </div>
-              <div>
-                <p className='text-fg/80 text-[11px]'>Time</p>
-                <p className='text-sm font-medium'>31m 25s</p>
-              </div>
-            </div>
-          </div>
+        <BentoBox className='col-span-1 row-span-3 flex h-[276px] flex-col justify-between md:col-span-3'>
+          <h6 className='font-medium'>Tech Stack</h6>
+          {loadingTechStack ? (
+            <div className='h-4 w-8 animate-pulse rounded-xl bg-gray-400'></div>
+          ) : (
+            <TechStackBox techStack={techStack} />
+          )}
+          {error && <p>Error loading tech stack</p>}
         </BentoBox>
         <BentoBox className='col-span-1 row-span-3 h-[276px] md:col-span-3'>
           <PixelPlayground />
@@ -137,9 +139,6 @@ export default function BentoGridSection() {
               <IconCircleArrowRight size={22} strokeWidth={1.5} />
             </span>
           </button>
-        </BentoBox>
-        <BentoBox className='row-span-[1.5] col-span-1 h-[72px] md:col-span-3'>
-          open to work
         </BentoBox>
         <BentoBox className='col-span-1 row-span-4 h-[276px] md:col-span-6 lg:col-span-6'>
           The Battlestation
