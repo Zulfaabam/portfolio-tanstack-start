67ecb845a906b1b00d9804558d6213de72272c9a
Zulfaabam <zulfafatahakbar@gmail.com>
2026-06-17 20:59:05 +0700

    fix: section padding on 1280px device-width

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -11,7 +11,7 @@ export default function Section({ children, id, className }: SectionProps) {
   return (
     <section
       className={cn(
-        'mx-auto max-w-5xl px-4 py-12 xl:max-w-7xl xl:px-0 xl:py-20',
+        'mx-auto max-w-5xl px-4 py-12 xl:max-w-7xl xl:py-20 min-[1300px]:px-0',
         className,
       )}
       id={id}
